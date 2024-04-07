4136e4e3d61ac9416bf069965b9ef6c8fd37789e
Romesh Abeyawardena <romesh.a@outlook.com>
2024-04-07 18:33:39 +0100

    Added multi-select component

--- ui/src/components/GroupManagement.vue
+++ ui/src/components/GroupManagement.vue
@@ -1,27 +1,32 @@
 <script setup lang="ts">
     import { storeToRefs } from 'pinia';
     import { useEntryGroupingStore } from '../stores/EntryGroupingStore';
-    import { useEntryStore } from '../stores/entryStore';
-    import { computed } from 'vue';
+    import { Button} from "primevue/button";
+    import { ref, computed } from 'vue';
+    import MultiSelect from 'primevue/multiselect';
+import { IEntryGroup } from '../models/EntryGroup';
 
     const props = defineProps({
         entryId: { type: String, required:true }
     });
-
-    const entryStore = useEntryStore();
-
+    const currentGroups = ref<Array<IEntryGroup>>([]);
     const entryGroupingStore = useEntryGroupingStore();
     const { groups } = storeToRefs(entryGroupingStore);
     const entryGroups = computed(() => {
-        return groups.value.filter(g => g.entryIds.some(e => e == props.entryId)).flatMap(h => entryStore.getEntries(h.entryIds));
+        return groups.value.filter(g => g.entryIds.some(e => e == props.entryId));
     });
 </script>
 <template>
     <div>
         <h4>Groups</h4>
         <!-- List the groups-->
+        <MultiSelect    option-label="name" 
+                        v-model="currentGroups" 
+                        :options="entryGroups" 
+                        placeholder="Group" />
     </div>
     <div>
         <!-- Modify groups-->
+        <Button label="Save groups" icon="pi pi-disk"></Button>
     </div>
 </template>
