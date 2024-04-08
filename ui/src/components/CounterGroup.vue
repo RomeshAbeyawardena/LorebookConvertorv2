afac2aad631c718cc6b0663c22f968ae75f6a2a1
Romesh Abeyawardena <romesh.a@outlook.com>
2024-04-08 22:31:31 +0100

    Added custom fieldset

---
+++ ui/src/components/CounterGroup.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Fieldset from 'primevue/fieldset';
+defineProps({
+    badgeValue: String,
+    legend: String,
+    toggleIcon: String,
+});
+</script>
+<template>
+    <Fieldset class="custom-fieldset" :collapsed="true" toggleable>
+        <template #legend>
+            <div class="flex justify-content-between align-items-center flex-wrap">
+                <h3 class="flex mr-2">{{ legend }}</h3>
+                <div class="flex flex-grow-1"></div>
+                <div class="flex align-items-center">
+                    <Badge class="flex align-items-center" :value="badgeValue" />
+                </div>
+            </div>
+        </template>
+        <template #togglericon>
+            <div class="flex mr-2">
+                <i :class="toggleIcon"></i>
+            </div>
+        </template>
+        <slot></slot>
+    </Fieldset>
+</template>
