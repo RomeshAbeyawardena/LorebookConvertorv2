c73c7c4725feb5b92c4235e71418704e2e6517b2
Romesh Abeyawardena <romesh.a@outlook.com>
2024-04-15 21:44:17 +0100

    added exception

--- backend/Lorebook.Convertor/Program.cs
+++ backend/Lorebook.Convertor/Program.cs
@@ -3,22 +3,28 @@ using Lorebook.Convertor.Domain;
 using System.Text.Json;
 
 var path = args.FirstOrDefault();
-
-if (string.IsNullOrWhiteSpace(path))
+try
 {
-    throw new NullReferenceException();
-}
+    if (string.IsNullOrWhiteSpace(path))
+    {
+        throw new NullReferenceException();
+    }
 
-if(!File.Exists(path))
+    if (!File.Exists(path))
+    {
+        throw new FileNotFoundException("Unable to find file");
+    }
+
+    var lorebook = DeserialisedLorebook.ParseJson(path);
+
+    var fileNameWithExt = Path.GetFileName(path);
+    var fileName = Path.GetFileNameWithoutExtension(path);
+    var basePath = path.Replace(fileNameWithExt, string.Empty);
+    Console.WriteLine(basePath);
+    File.WriteAllText($"{Path.Combine(basePath, fileName)}.json",
+        JsonSerializer.Serialize(lorebook));
+}
+catch(Exception exception)
 {
-    throw new FileNotFoundException("Unable to find file");
+    Console.Error.WriteLine(exception);
 }
-
-var lorebook = DeserialisedLorebook.ParseJson(path);
-
-var fileNameWithExt = Path.GetFileName(path);
-var fileName = Path.GetFileNameWithoutExtension(path);
-var basePath = path.Replace(fileNameWithExt, string.Empty);
-Console.WriteLine(basePath);
-File.WriteAllText($"{Path.Combine(basePath, fileName)}.json", 
-    JsonSerializer.Serialize(lorebook));
