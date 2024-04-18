1db415141747c2dd8788ef45929df2405133a01e
Romesh Abeyawardena <romesh.a@outlook.com>
2024-04-18 19:52:02 +0100

    wip

---
+++ backend/Lorebook.Convertor.Infrastructure/AccessTokens/IAccessTokenRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Lorebook.Convertor.Infrastructure.AccessTokens;
+
+public interface IRepository<T>
+    where T: class
+{
+    Task<T> Find(CancellationToken cancellationToken, params object[] keys);
+    Task<T> Upsert(AccessToken accessToken, CancellationToken cancellationToken);
+}
+
+public interface IAccessTokenRepository : IRepository<AccessToken>
+{
+    Task<AccessToken> GetAccessToken(string key, CancellationToken cancellationToken);
+    /// <summary>
+    /// Gets a list of access tokens for a credential
+    /// </summary>
+    /// <param name="credentialId">The credential ID</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<IEnumerable<AccessToken>> GetAccessTokensAsync(Guid credentialId, 
+        CancellationToken cancellationToken);
+}
