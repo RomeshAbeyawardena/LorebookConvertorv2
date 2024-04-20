8bb44803485776bf57f59281e80c7a11895c1b7d
Romesh Abeyawardena <romesh.a@outlook.com>
2024-04-20 23:03:02 +0100

    Implemented AccessTokenRepository

--- backend/Lorebook.Convertor.Infrastructure/AccessTokens/IAccessTokenRepository.cs
+++ backend/Lorebook.Convertor.Infrastructure/AccessTokens/IAccessTokenRepository.cs
@@ -1,4 +1,8 @@
-﻿using HSINet.Shared.Transactional;
+﻿using HSINet.Shared.Infrastructure;
+using HSINet.Shared.Infrastructure.Interceptors.Providers;
+using HSINet.Shared.Transactional;
+using HSINet.Shared.TypeCache;
+using System.Data.Entity;
 
 namespace Lorebook.Convertor.Infrastructure.AccessTokens;
 
@@ -13,4 +17,27 @@ public interface IAccessTokenRepository : IRepository<AccessToken>
     /// <returns></returns>
     Task<IEnumerable<AccessToken>> GetAccessTokensAsync(Guid credentialId, 
         CancellationToken cancellationToken);
+}
+
+public class AccessTokenRepository(CredentialDbContext context,
+    IRepositoryInterceptorFactoryProvider repositoryInterceptorFactoryProvider,
+    ITypeCacheProvider typeCacheProvider)
+    : RepositoryBase<AccessToken, CredentialDbContext>(context,
+        repositoryInterceptorFactoryProvider, typeCacheProvider),
+            IAccessTokenRepository
+{
+    public async Task<AccessToken> GetAccessToken(string key, CancellationToken cancellationToken)
+    {
+        return await Context.AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Key == key, cancellationToken);
+    }
+
+    public async Task<IEnumerable<AccessToken>> GetAccessTokensAsync(Guid credentialId, CancellationToken cancellationToken)
+    {
+        return await Context.AsNoTracking()
+            .Where(c => c.CredentialId == credentialId)
+            .Take(100)
+            .AsNoTracking()
+            .ToArrayAsync(cancellationToken);
+    }
 }
--- backend/Lorebook.Convertor.Infrastructure/CredentialDbContext.cs
+++ backend/Lorebook.Convertor.Infrastructure/CredentialDbContext.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HSINet.Shared.Transactional;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lorebook.Convertor.Infrastructure;
 
-public class CredentialDbContext : DbContext
+public class CredentialDbContext(DbContextOptions<CredentialDbContext> options) 
+    : DbContext(options), IUnitOfWork
 {
-
+    public DbSet<AccessTokens.AccessToken> AccessTokens { get; set; }
+    public DbSet<Credentials.Credential> Credentials { get; set; }
+    public DbSet<Permissions.Permission> Permissions { get; set; }
 }
